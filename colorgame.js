863609878acdc85d627c9fb64b5b61a70bc58728
xaviguasch <xaviguasch@gmail.com>
2018-12-14 19:05:43 +0100

    adding different difficulty levels

--- colorgame.js
+++ colorgame.js
@@ -1,4 +1,5 @@
-let colors = generateRandomColors(6)
+let numSquares = 6
+let colors = generateRandomColors(numSquares)
 
 const squares = document.querySelectorAll(".square")
 let pickedColor = pickColor()
@@ -7,10 +8,43 @@ const messageDisplay = document.querySelector("#message")
 
 const h1 = document.querySelector("h1")
 const resetButton = document.querySelector("#reset")
+const easyBtn = document.querySelector("#easyBtn")
+const hardBtn = document.querySelector("#hardBtn")
+
+easyBtn.addEventListener('click', function() {
+    hardBtn.classList.remove("selected")
+    easyBtn.classList.add("selected")  
+    numSquares = 3
+    colors = generateRandomColors(numSquares)
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor
+
+    for(let i = 0; i < squares.length; i ++) {
+        if (colors[i]) {
+            squares[i].style.background = colors[i]
+        } else {
+            squares[i].style.display = "none"
+        }
+    }
+
+})
+hardBtn.addEventListener('click', function() {
+    easyBtn.classList.remove("selected")
+    hardBtn.classList.add("selected")
+    numSquares = 6 
+    colors = generateRandomColors(numSquares)
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor
+    
+    for(let i = 0; i < squares.length; i ++) {
+        squares[i].style.background = colors[i]
+        squares[i].style.display = "block"
+
+    }})
 
 resetButton.addEventListener('click', function() {
     // generate all new colors
-    colors = generateRandomColors(6)
+    colors = generateRandomColors(numSquares)
     // pick a new random color from array
     pickedColor = pickColor()
 
